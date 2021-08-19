49e5f858ee1857e28ae885dff28294f6e59e8862
mohsin106 <mafridi@gmail.com>
2021-08-19 18:01:08 -0400

    added and deleted some stuff

---
+++ dismissal-app/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+function Header() {
+    return (
+        <div>
+            <header>
+            <nav>
+                <h1>Hello a third time!</h1>
+                <ul>
+                    <li>Thing 1</li>
+                    <li>Thing 2</li>
+                    <li>Thing 3</li>
+                </ul>
+            </nav>
+            </header>
+        </div>
+    )
+}
+export default Header

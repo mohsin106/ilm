0f31ecf90246e5742d6042605a0984230c317e96
mohsin106 <mafridi@gmail.com>
2021-08-28 17:50:30 -0400

    removed dismissal-app

--- dismissal-app/src/App.js
+++
@@ -1,9 +0,0 @@
-import React, {Component, useState, useEffect} from "react"
-import { render } from "react-dom"
-
-function App() {
-    render() {
-        <div>code goes here</div>
-    }
-}
-export default App

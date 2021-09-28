4538f9609f2664514d01292d3d8b723357a5b5f9
mohsin106 <mafridi@gmail.com>
2021-09-28 18:55:29 -0400

    testing

--- dismissal-app2/src/App.js
+++ dismissal-app2/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component, useEffect, useState} from "react"
+// import BuildStudents from "./components/BuildStudents"
+// import randomcolor from "randomcolor"
+// const { Component, useEffect, useState } = require("react")
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [color, setColor] = useState("")
+    const [students, setStudents] = useState([])
+    const [API_URL, setApiUrl] = useState("https://raw.githubusercontent.com/mohsin106/ilm/main/dismissal-app/students.json")
+
+    useEffect(async () => {
+      const response = await fetch(API_URL)
+      const data = await response.json()
+      const [item] = data.students
+      // setStudents(data)
+      // console.log(data)
+    }, [])
+
+
+
+    useEffect(() => {
+        fetch("https://swapi.dev/api/people/1/")
+    })
+    return (
+        <div>
+            <h1 style={{color: color}}>code goes here</h1>
+            <h1>Student name is {students}</h1>
+        </div>
+    )
 }
 
-export default App;
+export default App

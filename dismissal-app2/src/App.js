c905f2fd607b6adf8f24439bc82c94f6c969b1c6
mohsin106 <mafridi@gmail.com>
2021-09-30 20:28:58 -0400

    training

--- dismissal-app2/src/App.js
+++ dismissal-app2/src/App.js
@@ -1,31 +1,59 @@
-import React, {Component, useEffect, useState} from "react"
-// import BuildStudents from "./components/BuildStudents"
-// import randomcolor from "randomcolor"
-// const { Component, useEffect, useState } = require("react")
-
-
+import React, {useEffect, useState} from "react"
+import "./App.css"
+/* 
+We're going to create a custom hook to load students.
+*/
 function App() {
-    const [color, setColor] = useState("")
-    const [students, setStudents] = useState([])
     const [API_URL, setApiUrl] = useState("https://raw.githubusercontent.com/mohsin106/ilm/main/dismissal-app/students.json")
+    const [results, setResults] = useState([])
+    const [loading, setLoading] = useState(false)
 
     useEffect(async () => {
-      const response = await fetch(API_URL)
-      const data = await response.json()
-      const [item] = data.students
-      // setStudents(data)
-      // console.log(data)
+        async function fetchData() {
+
+        }
+        try {  
+            setLoading(true)
+            const response = await fetch(API_URL)
+            const data = await response.json()
+            // console.log(data)
+            setResults(
+                data.students.map(item => {
+                    return item
+                })
+            )
+        } finally {
+            setLoading(false)
+        }
     }, [])
+
 
 
 
     useEffect(() => {
         fetch("https://swapi.dev/api/people/1/")
     })
+
     return (
         <div>
-            <h1 style={{color: color}}>code goes here</h1>
-            <h1>Student name is {students}</h1>
+            {loading ? (
+                <h1>loading...</h1>
+            ) : (
+                <table>
+                    <thead>
+                        <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {results.map(item =>
+                            <tr key={item.id}><td>{item.firstName}</td><td>{item.lastName}</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            )}
         </div>
     )
 }
